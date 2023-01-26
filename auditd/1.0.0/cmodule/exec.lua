281f6d71f8a2ca08076b2c373071bf96b0a36430
Aleksey Nikitin <imsouz9@gmail.com>
2023-01-26 19:42:45 +0300

    auditd: add `exec` helper

---
+++ auditd/1.0.0/cmodule/exec.lua
@@ -0,0 +1,25 @@
+local function show_output(s)
+	local MAX_LENGTH = 1024
+	s = string.gsub(s, "\n", "\\n")
+	s = string.sub(s, 1, MAX_OUTPUT_LENGTH)
+	return s
+end
+
+-- Executes the given command in a shell.
+-- NOTE: stderr is redirected to stdout.
+--:: string -> ok::boolean, err::string?
+local function exec(cmd)
+	local f = io.popen("exec 2>&1;" .. cmd)
+	if not f then
+		return false, string.format("exec: %q: io.popen returned nil", cmd, err)
+	end
+	local output = f:read("a") or ""
+	local ok, status, code = f:close()
+	if not ok then
+		return false, string.format(
+			"exec: %q: %s=%d: %s", cmd, status, code, show_output(output))
+	end
+	return true
+end
+
+return exec
---
+++ auditd/1.0.0/cmodule/exec_spec.lua
@@ -0,0 +1,26 @@
+local exec = require "exec"
+
+local function string_contains(str, sub)
+	return string.find(str, sub, 1, true) ~= nil
+end
+
+describe("exec", function()
+	it("runs a given command", function()
+		local ok, err = exec("true")
+		assert(ok, "unexpected error: "..tostring(err))
+	end)
+
+	it("must fail unless the exit code is 0", function()
+		local ok, err = exec("echo ERROR; false")
+		assert(not ok)
+		assert(string_contains(err, 'exit=1: ERROR'),
+			"unexpected error message: "..tostring(err))
+	end)
+
+	test("a bad command", function()
+		local ok, err = exec("unknown-command")
+		assert(not ok)
+		assert(string_contains(err, "command not found"),
+			"unexpected error message: "..tostring(err))
+	end)
+end)
