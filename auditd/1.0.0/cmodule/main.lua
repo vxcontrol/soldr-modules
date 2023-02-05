77048d99cee5a751fdbc8615f06b5c2d4180f5d2
Aleksey Nikitin <imsouz9@gmail.com>
2023-02-05 15:02:09 +0300

    auditd: implement audit configuration watcher

--- auditd/1.0.0/cmodule/main.lua
+++ auditd/1.0.0/cmodule/main.lua
@@ -16,24 +16,13 @@ if not pm then
 	return "success"
 end
 
-local function setup_audit()
-	local ok, err = audit.setup(pm, {
-		audit.file_auditd_service_override,
-		audit.file_auditd_conf,
-		audit.file_audit_rules,
-	})
-	if not ok then __log.error(err) end
-end
-
-local check_interval_sec
-local last_check = 0
+local watcher = require("watcher").new()
 
 local function update_config()
 	local c = cjson.decode(__config.get_current_config())
-	check_interval_sec = c.check_interval_sec
 	audit.file_auditd_conf:set(c.auditd_conf)
 	audit.file_audit_rules:set(c.audit_rules)
-	last_check = 0
+	watcher:reset(c.check_interval)
 end
 update_config()
 
@@ -44,12 +33,12 @@ __api.add_cbs{
 	end,
 }
 
-while not __api.is_close() do
-	local now = os.time()
-	if now - last_check > check_interval_sec then
-		last_check = now
-		setup_audit()
-	end
-	__api.await(1000)
-end
+watcher:run(function()
+	local ok, err = audit.setup(pm, {
+		audit.file_auditd_service_override,
+		audit.file_auditd_conf,
+		audit.file_audit_rules,
+	})
+	if not ok then __log.error(err) end
+end)
 return "success"
