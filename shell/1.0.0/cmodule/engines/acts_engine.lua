9640b655ffd8eba65a5773b4c652389987da8217
jnovikov <johnnovikov0@gmail.com>
2023-02-09 23:54:01 +0000

    Shell: Implemented module.
    This change introduces new module that allow to spawn interactive
    web-shell on agent.
    Module supports Windows, Linux and MacOS.

---
+++ shell/1.0.0/cmodule/engines/acts_engine.lua
@@ -0,0 +1,298 @@
+require("yaci")
+require("strict")
+require("engines.base_engine")
+
+local ffi  = require("ffi")
+local fs = require("fs")
+local time = require("time")
+local cjson   = require("cjson.safe")
+local glue = require("glue")
+local b64 = require("libb64")
+
+if ffi.os == "Windows" then
+    require("ptys.winpty")
+else
+    require("ptys.unixpty")
+end
+
+
+CActsEngine = newclass("CActsEngine", CBaseEngine)
+
+--[[
+    cfg top keys:
+    * config - module arguments (hard limits)
+]]
+function CActsEngine:init(cfg)
+    __log.debug("init CActsEngine object")
+    assert(type(cfg) == "table", "configuration object has invalid type")
+
+    cfg.engine = "acts_engine"
+    self.super:init(cfg)
+
+    -- initialization of object after base class constructing
+    self:update_config_cb()
+
+    self.process_map = {}
+    self.winpty = nil
+end
+
+-- in: nil
+-- out: nil
+function CActsEngine:free()
+    __log.debug("finalize CActsEngine object")
+
+    -- here will be triggered after closing vxproto object (destructor of the state)
+end
+
+-- in: nil
+-- out: number
+--      amount of milliseconds timeout to wait next call of the timer_cb
+function CActsEngine:timer_cb()
+    -- __log.debug("timer_cb CActsEngine")
+
+    local acts_engine = CActsEngine:cast(self)
+
+    if next(acts_engine.process_map) == nil then
+        -- process table is empty
+        return 250
+    end
+
+    acts_engine:check_running_processes()
+
+    return 100
+end
+
+function CActsEngine:check_running_processes()
+    for k, proc_info in pairs(self.process_map) do
+        -- check for process output
+        local stdout_data, successful = proc_info.pty:get_data(10)
+
+        if successful then
+            if stdout_data ~= '' then
+                self.process_map[k].last_active = time.time()
+
+                local server_src, browser_src = self:from_process_key(k)
+
+                local event_data = {
+                    out = stdout_data,
+                    retaddr = browser_src,
+                }
+                __api.send_data_to(server_src, cjson.encode(event_data))
+            else
+                -- Check if process obsolete and can be killed.
+                local current_time = time.time()
+                local last_active_time = proc_info.last_active
+
+                -- 15 minutes passed since shell was active.
+                if current_time - last_active_time > 15 * 60 then
+                    __log.infof('killing inactive terminal session for key: %s', k)
+                    proc_info.pty:close()
+                    self.process_map[k] = nil
+                end
+            end
+        else
+            -- Read failed because of the error. Should kill the terminal to avoid errors.
+            __log.infof('killing terminal session for key: %s', k)
+            proc_info.pty:close()
+            self.process_map[k] = nil
+        end
+    end
+end
+
+-- in: nil
+-- out: nil
+function CActsEngine:quit_cb()
+    -- here will be triggered before closing vxproto object and destroying the state
+    __log.debug("quit_cb CActsEngine")
+    local acts_engine = CActsEngine:cast(self)
+
+    for k, proc_info in pairs(acts_engine.process_map) do
+        proc_info.pty:close()
+    end
+    acts_engine.process_map = {}
+end
+
+-- in: string
+--      destination token (string) of server module side
+-- out: nil
+function CActsEngine:agent_connected_cb(dst)
+    __log.debugf("agent_connected_cb CActsEngine with token '%s'", dst)
+end
+
+-- in: string
+--      destination token (string) of server module side
+-- out: nil
+function CActsEngine:agent_disconnected_cb(dst)
+    __log.debugf("agent_disconnected_cb CActsEngine with token '%s'", dst)
+end
+
+-- in: nil
+-- out: nil
+function CActsEngine:update_config_cb()
+    __log.debug("update_config_cb CActsEngine")
+
+    -- actual current configuration contains into next fields
+    -- self.config.actions
+    -- self.config.events
+    -- self.config.module
+end
+
+-- in: string, string
+--      source token (string) of sender module side
+--      data payload (string) as a custom string serialized struct object (json)
+-- out: boolean
+--      result of data processing from business logic
+function CActsEngine:recv_data_cb(src, data)
+    __log.debugf("perform custom logic for data with payload '%s' from '%s'", data, src)
+    local p = cjson.decode(data)
+    local retaddr = p['retaddr']
+
+    local acts_engine = CActsEngine:cast(self)
+
+    return acts_engine:provide_input(src, retaddr, p['i'])
+end
+
+-- in: string, string, string
+--      source token (string) of sender module side
+--      file path (string) on local FS where received file was stored
+--      file name (string) is a original file name which was set on sender side
+-- out: boolean
+--      result of file processing from business logic
+function CActsEngine:recv_file_cb(src, path, name)
+    __log.debugf("perform custom logic for file with path '%s' and name '%s' from '%s'", path, name, src)
+    return true
+end
+
+-- in: string, string, table
+--      source token (string) of sender module side
+--      action name (string) to execute it into the acts_engine
+--      action data (table) as a arguments to execute action via acts_engine
+--        e.x. {"data": {"key": "val"}, "actions": ["mod1.act1"]}
+-- out: boolean
+--      result of action processing from business logic
+function CActsEngine:recv_action_cb(src, data, name)
+    __log.debugf("perform custom logic for action '%s' from '%s'", name, src)
+
+    local acts_engine = CActsEngine:cast(self)
+
+    local retaddr = data['retaddr']
+
+    if name == 'shell_start' then
+        return acts_engine:exec_cmd(src, retaddr)
+    end
+
+    if name == 'shell_stop' then
+        return acts_engine:stop_shell(src, retaddr)
+    end
+
+    return false
+end
+
+function CActsEngine:provide_input(server_src, browser_src, input)
+    local proc_key = self:process_key(server_src, browser_src)
+    local proc_info = self.process_map[proc_key]
+    if proc_info == nil then
+        __log.errorf("failed to find process in map for (%s, %s)", server_src, browser_src)
+        return false
+    end
+
+    proc_info.pty:send_input(input)
+    local stdout_data, successful = proc_info.pty:get_data(50)
+    if stdout_data ~= '' and successful then
+        local server_src, browser_src = self:from_process_key(proc_key)
+
+        local event_data = {
+            out = stdout_data,
+            retaddr = browser_src,
+        }
+        __api.send_data_to(server_src, cjson.encode(event_data))
+    end
+    
+    self.process_map[proc_key].last_active = time.time()
+
+    return true
+end
+
+-- in: any
+-- out: boolean
+function CActsEngine:exec_cmd(server_src, browser_src)
+    __log.debugf("execCmd CActsEngine with args %s, %s", server_src, browser_src)
+
+    local proc_key = self:process_key(server_src, browser_src)
+    local proc_info = self.process_map[proc_key]
+
+    local shell_cmd = self:get_shell_cmd()
+
+    __log.infof("spawning shell %s", shell_cmd)
+
+    if proc_info == nil then
+        -- create new process and store it inside map.
+        local pty = self:get_pty()
+        if pty:start(shell_cmd) == false then
+            pty:close()
+            return false
+        end
+
+        proc_info = {
+            pty = pty,
+            last_active = time.time(),
+        }
+
+        self.process_map[proc_key] = proc_info
+        return true
+    end
+
+    proc_info.pty:close()
+    proc_info.pty = self:get_pty()
+
+    if proc_info.pty:start(shell_cmd) == false then
+        proc_info.pty:close()
+        return false
+    end
+
+    proc_info.last_active = time.time()
+    self.process_map[proc_key] = proc_info
+    return true
+end
+
+function CActsEngine:get_shell_cmd()
+    if ffi.os == "Windows" then 
+        return "cmd"
+    end
+
+    local env_shell = os.getenv("SHELL")
+    if env_shell ~= nil then
+        return env_shell
+    end
+
+    return "/bin/sh"
+end
+
+function CActsEngine:get_pty()
+    if ffi.os == "Windows" then 
+        return CWinPty()
+    end
+    return CUnixPty()
+end
+
+function CActsEngine:stop_shell(server_src, browser_src)
+    local proc_key = self:process_key(server_src, browser_src)
+    local proc_info = self.process_map[proc_key]
+
+    if proc_info == nil then
+        return false
+    end
+
+    proc_info.pty:close()
+    self.process_map[proc_key] = nil
+    return true
+end
+
+function CActsEngine:process_key(server_src, browser_src)
+    return cjson.encode({server_src= server_src, browser_src= browser_src})
+end
+
+function CActsEngine:from_process_key(key)
+    local decoded_key = cjson.decode(key)
+    return decoded_key.server_src, decoded_key.browser_src
+end
---
+++ shell/1.0.0/cmodule/ptys/unixpty.lua
@@ -0,0 +1,265 @@
+require("yaci")
+require("ptys.pty")
+
+local ffi  = require("ffi")
+
+ffi.cdef [[
+    typedef int mode_t;
+    typedef int32_t pid_t;
+    typedef uint32_t nfds_t;
+
+    int kill(pid_t pid, int sig);
+    pid_t wait(int *wstatus);
+    pid_t fork(void);
+    pid_t setsid(void);
+
+    int execvp(const char *file, char *const argv[]);
+
+    int open(const char* pathname, int flags, ...);
+
+    int close(int fd);
+    int dup2(int oldfd, int newfd);
+
+    ssize_t read(int fd, void *buf, size_t count);
+
+    struct winsize {
+        unsigned short ws_row;
+        unsigned short ws_col;
+        unsigned short ws_xpixel;   /* unused */
+        unsigned short ws_ypixel;   /* unused */
+    };
+
+    int ioctl(int fd, int cmd, ...);
+    char* ptsname(int fd);
+    int grantpt(int fd);
+    int unlockpt(int fd);
+
+    int openpty(int* aprimary, int* areplica, char* name,
+              void* termp,
+              const struct winsize *winp);
+    int login_tty(int fd);
+
+    struct pollfd {
+        int fd;
+        short int events;
+        short int revents;
+    };
+    int poll(struct pollfd *fds, nfds_t nfds, int timeout);
+]]
+
+EBUSY = 16
+
+CUnixPty = newclass("CUnixPty", CPty)
+
+function CUnixPty:init()
+    __log.debug("init CUnixPty")
+    self.pty = nil
+    self.shell_pid = 0
+    self.closed = false
+end
+
+function CUnixPty:start(cmd)
+    __log.debug("start CUnixPty")
+
+    local winsize = ffi.new("struct winsize [1]")
+    winsize[0].ws_row = 35
+    winsize[0].ws_col = 120
+
+    if ffi.os == "OSX" then
+        return self:start_bsd(cmd, winsize)
+    end
+
+    -- openpty(),loginpty() are only defined in BSD systems by default, so need to differentiate.
+    return self:start_linux(cmd, winsize)
+end
+
+function CUnixPty:start_linux(cmd, winsize)
+    local O_RDWR = 0x2
+    local O_NOCTTY = 0x100
+
+    -- print("test")
+    -- local path = 
+    -- local path_c_str = ffi.new("char[?]", #path + 1)
+    -- ffi.copy(path_c_str, path)
+
+    local primary_fd = ffi.C.open("/dev/ptmx", O_RDWR, 0)
+    
+    -- print('test 123')
+    if primary_fd == -1 then
+        __log.errorf("cUnixPty: failed to open /dev/ptmx with error %d", ffi.errno())
+        return false
+    end
+
+    local grant_res = ffi.C.grantpt(primary_fd)
+    if grant_res == -1 then
+        __log.errorf("cUnixPty: granpt(%d) failed with error: %d", primary_fd, ffi.errno())
+        return false
+    end
+
+    local unlock_res = ffi.C.unlockpt(primary_fd)
+    if unlock_res == -1 then
+        __log.errorf("cUnixPty: unlockpt(%d) failed with error: %d", primary_fd, ffi.errno())
+        return false
+    end
+
+    local ptsname = ffi.C.ptsname(primary_fd)
+    if ptsname == nil then
+        __log.errorf("cUnixPty: failed to get ptsname for follower tmux with error %d", ffi.errno())
+        return false
+    end
+
+    local replica_fd = ffi.C.open(ptsname, O_RDWR + O_NOCTTY, 0)
+    if replica_fd == -1 then
+        __log.errorf("cUnixPty: failed to open %s with error %d", ffi.string(ptsname), ffi.errno())
+        return false
+    end
+
+    local TIOCSWINSZ = 0x5414
+    
+
+    if ffi.C.ioctl(replica_fd, TIOCSWINSZ, winsize) == -1 then
+        __log.errorf("cUnixPty: failed to set windows size using ioctl(%d): %d", TIOCSWINSZ, ffi.errno())
+    end
+
+    self.pty = primary_fd
+    return self:spawn_shell(cmd, replica_fd)
+end
+
+function CUnixPty:start_bsd(cmd, winsize)
+    local primary = ffi.new("int[1]")
+    local replica = ffi.new("int[1]")
+
+    if ffi.C.openpty(primary, replica, nil, nil, winsize) == -1 then
+        __log.errorf("cUnixPty: failed to call openpty() with errno: %d", ffi.errno())
+        return false
+    end
+
+
+    self.pty = primary[0]
+    return self:spawn_shell(cmd, replica[0])
+end
+
+function CUnixPty:get_data(timeout)
+    local POLLIN = 0x0001
+    local POLLPRI = 0x0002
+
+    local to_poll = ffi.new("struct pollfd [1]")
+    to_poll[0].fd = self.pty
+    to_poll[0].events = POLLIN + POLLPRI
+
+    local num_ready = ffi.C.poll(to_poll, 1, timeout)
+
+    if num_ready > 0 then
+        local buffer=ffi.new("char[?]", 10240)
+        local read = ffi.C.read(self.pty, buffer, 10240)
+        if read == -1 and ffi.errno() ~= EBUSY then
+            __log.errorf("cUnixPty: failed to read bytes from tty: %d", ffi.errno())
+            return '', false
+        end
+
+        return ffi.string(buffer, read), true
+    end
+    return '', true
+end
+
+function CUnixPty:send_input(s)
+    local c_str = ffi.new("char[?]", #s + 1)
+    c_str[#s] = 0
+    ffi.copy(c_str, s)
+    local bytes_w = ffi.C.write(self.pty, c_str, #s)
+    if bytes_w == -1 then
+        __log.errorf("cUnixPty: failed to write bytes to tty: %d", ffi.errno())
+    end
+end
+
+function CUnixPty:close()
+    if self.closed then
+        __log.infof("cUnixPty: attempt to close pty that is already closed")
+        return
+    end
+
+    if self.pty ~= nil then
+        if ffi.C.close(self.pty) == -1 then
+            __log.errorf("cUnixPty: failed to close pty fd with error %d", ffi.errno())
+        end
+        self.pty = nil
+    end
+
+    -- send a sigkill
+    local status = ffi.new("int [1]")
+    if self.shell_pid > 0 then
+        if ffi.C.kill(-self.shell_pid, 9) == -1 then
+            __log.errorf("cUnixPty: failed to send SIGKILL to process %d with error %d", -self.shell_pid, ffi.errno())
+            -- waiting for process may freeze the agent, so we should not wait if process wasn't killed.
+            return
+        end
+        self.shell_pid = 0
+    end
+
+    if ffi.C.wait(status) == -1 then
+        __log.errorf("cUnixPty: failed to wait() with error %d", ffi.errno())
+    end
+
+    self.closed = true
+end
+
+function CUnixPty:spawn_shell(cmd, pty_fd)
+    local pid = ffi.C.fork()
+    if pid < 0  then
+        __log.errorf("fork failed with error %d", ffi.errno())
+        return false
+    elseif pid == 0 then -- child process
+        if ffi.C.close(self.pty) == -1 then
+            __log.errorf("cUnixPty: failed to close primary pty fd from child process, errno = %d", ffi.errno())
+        end
+
+        if self:login_tty(pty_fd) == -1 then
+            __log.errorf("cUnixPty: login_tty failed with errno: %d", ffi.errno())
+        end
+
+
+        local argv = ffi.new("const char* [?]", 2)
+        argv[0] = cmd
+        argv[1] = nil
+        local char_p_k_p_t   = ffi.typeof('char * const *')
+
+        local res = ffi.C.execvp(cmd, ffi.cast(char_p_k_p_t, argv))
+        if res == -1 then
+            __log.errorf("cUnixPty: execvp(%s) failed with errorno =  %d", cmd, ffi.errno())
+            return false
+        end
+        -- this line should never be reached.
+        return true
+    end
+    self.shell_pid = pid
+    ffi.C.close(pty_fd)
+    return true
+end
+
+function CUnixPty:login_tty(pty_fd)
+    if ffi.os == "OSX" then
+        return ffi.C.login_tty(pty_fd)
+    end
+
+    if ffi.C.setsid() == -1 then
+        __log.errorf("cUnixPty: setsid failed with errno: %d", ffi.errno())
+        return -1
+    end
+
+    local TIOCSCTTY = 0x540E
+
+    if ffi.C.ioctl(pty_fd, TIOCSCTTY, 0) == -1 then
+        __log.errorf("cUnixPty: failed to ioctl with error %d", ffi.errno())
+        return -1
+    end
+
+    while (ffi.C.dup2(pty_fd, 0) == -1) and ffi.errno() == EBUSY do end
+    while (ffi.C.dup2(pty_fd, 1) == -1) and ffi.errno() == EBUSY do end
+    while (ffi.C.dup2(pty_fd, 2) == -1) and ffi.errno() == EBUSY do end
+
+    if (pty_fd > 2) then
+        ffi.C.close(pty_fd)
+    end
+
+    return 0
+end
