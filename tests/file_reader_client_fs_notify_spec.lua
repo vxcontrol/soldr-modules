a0033c58fd9401977f0879137c3233ff2dbd7f5f
Egor Artemov <egor.artemov@cyberok.ru>
2023-02-13 21:40:59 +0800

    Create and remove test files for file_reader on test run

--- tests/file_reader_client_fs_notify_spec.lua
+++ tests/file_reader_client_fs_notify_spec.lua
@@ -1,7 +1,13 @@
 require("busted.runner")()
-package.path = package.path .. ";./file_reader/1.0.0/cmodule/?.lua"
-local fs_notify = require("fs_notify")
+
 local ffi = require("ffi")
+local path = require("path")
+local fs = require("lfs")
+local glue = require("glue")
+
+package.path = package.path .. ";./file_reader/1.0.0/cmodule/?.lua"
+
+local fs_notify = require("fs_notify")
 
 describe("is_glob_pattern", function()
     it("returns false for empty strings", function()
@@ -21,8 +27,9 @@ end)
 
 describe("is_filename_match_pattern", function()
     if ffi.os ~= "Windows" then
-        pending("Linux")
-        pending("OSX")
+        it("always returns false", function()
+            assert.is_false(fs_notify.is_filename_match_pattern("test.txt", "*"))
+        end)
         return
     end
     it("returns true for empty pattern", function()
@@ -37,19 +44,39 @@ describe("is_filename_match_pattern", function()
 end)
 
 describe("find_all_files", function()
+    local tmp_dir = os.tmpname()
+    os.remove(tmp_dir)
+    local matching_files = {
+        path.combine(tmp_dir, "test1.txt"),
+        path.combine(tmp_dir, "test2.txt"),
+    }
+    local all_files = glue.merge(matching_files, { path.combine(tmp_dir, "other.txt") })
+    local all_tests_glob = path.combine(tmp_dir, "test*.txt")
+
+    setup(function()
+        fs.mkdir(tmp_dir)
+        for _, filename in ipairs(all_files) do
+            io.open(filename, "w"):close()
+        end
+    end)
+    teardown(function()
+        for _, filename in ipairs(all_files) do
+            os.remove(filename)
+        end
+        fs.rmdir(tmp_dir)
+    end)
+
     if ffi.os ~= "Windows" then
-        pending("Linux")
-        pending("OSX")
+        it("always returns empty set", function()
+            assert.are.same({}, fs_notify.find_all_files(all_tests_glob))
+        end)
         return
     end
     it("returns an empty list when there are no matching files", function()
-        local files = fs_notify.find_all_files("tests/data/file_reader_client/notmatching*.txt")
-
-        assert.are.same({}, files)
+        local not_matching_glob = path.combine(tmp_dir, "notmatching*.txt")
+        assert.are.same({}, fs_notify.find_all_files(not_matching_glob))
     end)
     it("returns all files matching pattern", function()
-        local files = fs_notify.find_all_files("tests/data/file_reader_client/test*.txt")
-
-        assert.are.same({ "tests/data/file_reader_client/test1.txt", "tests/data/file_reader_client/test2.txt" }, files)
+        assert.are.same(matching_files, fs_notify.find_all_files(all_tests_glob))
     end)
 end)
